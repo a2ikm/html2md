00ec87805e5f83a9aebe23dfa7a88df71be40311
Masato Ikeda <masato.ikeda@gmail.com>
2024-03-20 21:12:39 +0900

    Determine source file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <path>", args[0]);
+        std::process::exit(1);
+    }
+    let path = &args[1];
+
+    println!("path = {}", path);
 }
